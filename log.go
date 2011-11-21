2c958a22b8ad867c335648f7a53c5bc04f63ee41
Richard Crowley <r@rcrowley.org>
2011-11-21 21:34:22 +0000

    Fixing log formats.

--- log.go
+++ log.go
@@ -8,95 +8,57 @@ import (
 func Log(r Registry, interval int, l *log.Logger) {
 	for {
 		for name, c := range r.Counters() {
-			l.Printf("counter %s\n\tcount:\t%9d\n", name, c.Count())
+			l.Printf("counter %s\n", name)
+			l.Printf("  count:       %9d\n", c.Count())
 		}
 		for name, g := range r.Gauges() {
-			l.Printf("gauge %s\n\tvalue:\t%9d\n", name, g.Value())
+			l.Printf("gauge %s\n", name)
+			l.Printf("  value:       %9d\n", g.Value())
 		}
 		r.RunHealthchecks()
 		for name, h := range r.Healthchecks() {
-			l.Printf("healthcheck %s\n\terror: %v\n", name, h.Error())
+			l.Printf("healthcheck %s\n", name)
+			l.Printf("  error:       %v\n", h.Error())
 		}
 		for name, h := range r.Histograms() {
 			ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
-			l.Printf(
-				`histogram %s
-	count:	%9d
-	min:	%9d
-	max:	%9d
-	mean:	%12.2f
-	stddev:	%12.2f
-	median:	%12.2f
-	75%%:	%12.2f
-	95%%:	%12.2f
-	99%%:	%12.2f
-	99.9%%:	%12.2f
-`,
-				name,
-				h.Count(),
-				h.Min(),
-				h.Max(),
-				h.Mean(),
-				h.StdDev(),
-				ps[0],
-				ps[1],
-				ps[2],
-				ps[3],
-				ps[4],
-			)
+			l.Printf("histogram %s\n", name)
+			l.Printf("  count:       %9d\n", h.Count())
+			l.Printf("  min:         %9d\n", h.Min())
+			l.Printf("  max:         %9d\n", h.Max())
+			l.Printf("  mean:        %12.2f\n", h.Mean())
+			l.Printf("  stddev:      %12.2f\n", h.StdDev())
+			l.Printf("  median:      %12.2f\n", ps[0])
+			l.Printf("  75%%:         %12.2f\n", ps[1])
+			l.Printf("  95%%:         %12.2f\n", ps[2])
+			l.Printf("  99%%:         %12.2f\n", ps[3])
+			l.Printf("  99.9%%:       %12.2f\n", ps[4])
 		}
 		for name, m := range r.Meters() {
-			l.Printf(
-				`meter %s
-	count:	%9d
-	1-min:	%12.2f
-	5-min:	%12.2f
-	15-min:	%12.2f
-	mean:	%12.2f
-`,
-				name,
-				m.Count(),
-				m.Rate1(),
-				m.Rate5(),
-				m.Rate15(),
-				m.RateMean(),
-			)
+			l.Printf("meter %s\n", name)
+			l.Printf("  count:       %9d\n", m.Count())
+			l.Printf("  1-min rate:  %12.2f\n", m.Rate1())
+			l.Printf("  5-min rate:  %12.2f\n", m.Rate5())
+			l.Printf("  15-min rate: %12.2f\n", m.Rate15())
+			l.Printf("  mean rate:   %12.2f\n", m.RateMean())
 		}
 		for name, t := range r.Timers() {
 			ps := t.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
-			l.Printf(
-				`timer %s
-	count:	%9d
-	min:	%9d
-	max:	%9d
-	mean:	%12.2f
-	stddev:	%12.2f
-	median:	%12.2f
-	75%%:	%12.2f
-	95%%:	%12.2f
-	99%%:	%12.2f
-	99.9%%:	%12.2f
-	1-min:	%12.2f
-	5-min:	%12.2f
-	15-min:	%12.2f
-	mean:	%12.2f
-`,
-				name,
-				t.Count(),
-				t.Min(),
-				t.Max(),
-				t.Mean(),
-				t.StdDev(),
-				ps[0],
-				ps[1],
-				ps[2],
-				ps[3],
-				ps[4],
-				t.Rate1(),
-				t.Rate5(),
-				t.Rate15(),
-				t.RateMean(),
-			)
+			l.Printf("timer %s\n", name)
+			l.Printf("  count:       %9d\n", t.Count())
+			l.Printf("  min:         %9d\n", t.Min())
+			l.Printf("  max:         %9d\n", t.Max())
+			l.Printf("  mean:        %12.2f\n", t.Mean())
+			l.Printf("  stddev:      %12.2f\n", t.StdDev())
+			l.Printf("  median:      %12.2f\n", ps[0])
+			l.Printf("  75%%:         %12.2f\n", ps[1])
+			l.Printf("  95%%:         %12.2f\n", ps[2])
+			l.Printf("  99%%:         %12.2f\n", ps[3])
+			l.Printf("  99.9%%:       %12.2f\n", ps[4])
+			l.Printf("  1-min rate:  %12.2f\n", t.Rate1())
+			l.Printf("  5-min rate:  %12.2f\n", t.Rate5())
+			l.Printf("  15-min rate: %12.2f\n", t.Rate15())
+			l.Printf("  mean rate:   %12.2f\n", t.RateMean())
 		}
 		time.Sleep(int64(1e9) * int64(interval))
 	}
--- syslog.go
+++ syslog.go
@@ -47,23 +47,24 @@ func Syslog(r Registry, interval int, w *syslog.Writer) {
 			))
 		}
 		for name, t := range r.Timers() {
+			ps := t.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
 			w.Info(fmt.Sprintf(
 				"timer %s: count: %d min: %d max: %d mean: %.2f stddev: %.2f median: %.2f 75%%: %.2f 95%%: %.2f 99%%: %.2f 99.9%%: %.2f 1-min: %.2f 5-min: %.2f 15-min: %.2f mean: %.2f",
 				name,
-				h.Count(),
-				h.Min(),
-				h.Max(),
-				h.Mean(),
-				h.StdDev(),
+				t.Count(),
+				t.Min(),
+				t.Max(),
+				t.Mean(),
+				t.StdDev(),
 				ps[0],
 				ps[1],
 				ps[2],
 				ps[3],
 				ps[4],
-				m.Rate1(),
-				m.Rate5(),
-				m.Rate15(),
-				m.RateMean(),
+				t.Rate1(),
+				t.Rate5(),
+				t.Rate15(),
+				t.RateMean(),
 			))
 		}
 		time.Sleep(int64(1e9) * int64(interval))
