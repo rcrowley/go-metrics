42e760a2488e7febca02db8a622abef19d57569a
Richard Crowley <r@rcrowley.org>
2013-09-29 00:04:43 +0000

    Test case for #21.

--- meter_test.go
+++ meter_test.go
@@ -1,6 +1,9 @@
 package metrics
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestMeterZero(t *testing.T) {
 	m := NewMeter()
@@ -14,5 +17,20 @@ func TestMeterNonzero(t *testing.T) {
 	m.Mark(3)
 	if count := m.Count(); 3 != count {
 		t.Errorf("m.Count(): 3 != %v\n", count)
+	}
+}
+
+func TestMeterDecay(t *testing.T) {
+	m := &StandardMeter{
+		make(chan int64),
+		make(chan meterV),
+		time.NewTicker(1),
+	}
+	go m.arbiter()
+	m.Mark(1)
+	rateMean := m.RateMean()
+	time.Sleep(1)
+	if m.RateMean() >= rateMean {
+		t.Error("m.RateMean() didn't decrease")
 	}
 }
