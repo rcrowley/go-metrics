fe784710d01893a3777ad395918843ed229a4094
Alban Peignier <alban@tryphon.eu>
2013-11-07 10:30:30 +0100

    Use Histogram.Snapshot() to fill json

--- json.go
+++ json.go
@@ -19,17 +19,19 @@ func (r StandardRegistry) MarshalJSON() ([]byte, error) {
 			m.Check()
 			values["error"] = m.Error()
 		case Histogram:
-			ps := m.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
 			values["count"] = m.Count()
-			values["min"] = m.Min()
-			values["max"] = m.Max()
-			values["mean"] = m.Mean()
-			values["stddev"] = m.StdDev()
-			values["median"] = ps[0]
-			values["75%%"] = ps[1]
-			values["95%%"] = ps[2]
-			values["99%%"] = ps[3]
-			values["99.9%%"] = ps[4]
+
+			snapshot := m.Snapshot()
+
+			values["min"] = snapshot.Min()
+			values["max"] = snapshot.Max()
+			values["mean"] = snapshot.Mean()
+			values["stddev"] = snapshot.StdDev()
+			values["median"] = snapshot.Percentile(0.5)
+			values["75%%"] = snapshot.Percentile(0.75)
+			values["95%%"] = snapshot.Percentile(0.95)
+			values["99%%"] = snapshot.Percentile(0.99)
+			values["99.9%%"] = snapshot.Percentile(0.999)
 		case Meter:
 			values["count"] = m.Count()
 			values["1m.rate"] = m.Rate1()
