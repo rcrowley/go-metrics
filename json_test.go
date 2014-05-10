4d17aae18c35eac1104e0dae1e0cf80a2860505f
Richard Crowley <r@rcrowley.org>
2014-05-10 08:12:50 -0700

    Add a test for JSON encoding
    This is mostly as an example for how to use it.

---
+++ json_test.go
@@ -0,0 +1,18 @@
+package metrics
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+)
+
+func TestRegistryMarshallJSON(t *testing.T) {
+	b := &bytes.Buffer{}
+	enc := json.NewEncoder(b)
+	r := NewRegistry()
+	r.Register("counter", NewCounter())
+	enc.Encode(r)
+	if s := b.String(); "{\"counter\":{\"count\":0}}\n" != s {
+		t.Fatalf(s)
+	}
+}
