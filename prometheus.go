632bcf56773ea1d64f931a33deecebcc9d524637
deathowl <bcsergo@hortonworks.com>
2016-09-08 17:56:21 +0200

    fmt

--- prometheus.go
+++ prometheus.go
@@ -1,27 +1,27 @@
 package metrics
 
 import (
-	"strings"
 	"github.com/prometheus/client_golang/prometheus"
+	"strings"
 )
 
 // PrometheusConfig provides a container with config parameters for the
 // Prometheus Exporter
 
 type PrometheusConfig struct {
-	namespace string
-	Registry Registry // Registry to be exported
-	subsystem string
+	namespace    string
+	Registry     Registry // Registry to be exported
+	subsystem    string
 	promRegistry prometheus.Registerer //Prometheus registry
 }
 
 // NewPrometheusProvider returns a Provider that produces Prometheus metrics.
 // Namespace and subsystem are applied to all produced metrics.
-func NewPrometheusProvider(r Registry, namespace string, subsystem string, promRegistry prometheus.Registerer) *PrometheusConfig{
+func NewPrometheusProvider(r Registry, namespace string, subsystem string, promRegistry prometheus.Registerer) *PrometheusConfig {
 	return &PrometheusConfig{
-		namespace: namespace,
-		subsystem: subsystem,
-		Registry: r,
+		namespace:    namespace,
+		subsystem:    subsystem,
+		Registry:     r,
 		promRegistry: promRegistry,
 	}
 }
@@ -34,7 +34,7 @@ func (c *PrometheusConfig) flattenKey(key string) string {
 	return key
 }
 
-func (c *PrometheusConfig) gaugeFromNameAndValue(name string, val float64) prometheus.Gauge{
+func (c *PrometheusConfig) gaugeFromNameAndValue(name string, val float64) prometheus.Gauge {
 	gauge := prometheus.NewGauge(prometheus.GaugeOpts{
 		Namespace: c.flattenKey(c.namespace),
 		Subsystem: c.flattenKey(c.subsystem),
@@ -56,10 +56,10 @@ func (c *PrometheusConfig) UpdatePrometheusMetrics() error {
 			c.gaugeFromNameAndValue(name, float64(metric.Count()))
 		case Gauge:
 		case GaugeFloat64:
-			 c.gaugeFromNameAndValue(name, float64(metric.Value()))
+			c.gaugeFromNameAndValue(name, float64(metric.Value()))
 		case Histogram:
 			samples := metric.Snapshot().Sample().Values()
-			lastSample :=  samples[len(samples)-1]
+			lastSample := samples[len(samples)-1]
 			c.gaugeFromNameAndValue(name, float64(lastSample))
 		case Meter:
 		case Timer:
