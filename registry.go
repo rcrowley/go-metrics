e2a30da93bd89e62b9104878554af288e673604d
Sergey Kacheev <skacheev@yandex-team.ru>
2017-09-17 22:38:33 +0700

    add Dump in the Registry interface

--- registry.go
+++ registry.go
@@ -40,6 +40,9 @@ type Registry interface {
 	// Run all registered healthchecks.
 	RunHealthchecks()
 
+	// Dump all metrics.
+	Dump() map[string]map[string]interface{}
+
 	// Unregister the metric with the given name.
 	Unregister(string)
 
@@ -107,6 +110,67 @@ func (r *StandardRegistry) RunHealthchecks() {
 			h.Check()
 		}
 	}
+}
+
+// Dump all the metrics in the Registry
+func (r *StandardRegistry) Dump() map[string]map[string]interface{} {
+	data := make(map[string]map[string]interface{})
+	r.Each(func(name string, i interface{}) {
+		values := make(map[string]interface{})
+		switch metric := i.(type) {
+		case Counter:
+			values["count"] = metric.Count()
+		case Gauge:
+			values["value"] = metric.Value()
+		case GaugeFloat64:
+			values["value"] = metric.Value()
+		case Healthcheck:
+			values["error"] = nil
+			metric.Check()
+			if err := metric.Error(); nil != err {
+				values["error"] = metric.Error().Error()
+			}
+		case Histogram:
+			h := metric.Snapshot()
+			ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
+			values["count"] = h.Count()
+			values["min"] = h.Min()
+			values["max"] = h.Max()
+			values["mean"] = h.Mean()
+			values["stddev"] = h.StdDev()
+			values["median"] = ps[0]
+			values["75%"] = ps[1]
+			values["95%"] = ps[2]
+			values["99%"] = ps[3]
+			values["99.9%"] = ps[4]
+		case Meter:
+			m := metric.Snapshot()
+			values["count"] = m.Count()
+			values["1m.rate"] = m.Rate1()
+			values["5m.rate"] = m.Rate5()
+			values["15m.rate"] = m.Rate15()
+			values["mean.rate"] = m.RateMean()
+		case Timer:
+			t := metric.Snapshot()
+			ps := t.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
+			values["count"] = t.Count()
+			values["min"] = t.Min()
+			values["max"] = t.Max()
+			values["mean"] = t.Mean()
+			values["stddev"] = t.StdDev()
+			values["median"] = ps[0]
+			values["75%"] = ps[1]
+			values["95%"] = ps[2]
+			values["99%"] = ps[3]
+			values["99.9%"] = ps[4]
+			values["1m.rate"] = t.Rate1()
+			values["5m.rate"] = t.Rate5()
+			values["15m.rate"] = t.Rate15()
+			values["mean.rate"] = t.RateMean()
+		}
+		data[name] = values
+	})
+	return data
 }
 
 // Unregister the metric with the given name.
@@ -214,6 +278,11 @@ func (r *PrefixedRegistry) Register(name string, metric interface{}) error {
 // Run all registered healthchecks.
 func (r *PrefixedRegistry) RunHealthchecks() {
 	r.underlying.RunHealthchecks()
+}
+
+// Dump all the metrics in the Registry
+func (r *PrefixedRegistry) Dump() map[string]map[string]interface{} {
+	return r.underlying.Dump()
 }
 
 // Unregister the metric with the given name. The name will be prefixed.
